78207481bb1b852c7ed2109dad4aabb07510869e
NRottmann <Nils.Rottmann@tuhh.de>
2017-05-08 18:12:39 +0200

    Adding comments and deleting old comments

--- src/gazebo_to_mavros.cpp
+++ src/gazebo_to_mavros.cpp
@@ -1,9 +1,17 @@
+/*
+*   gazebo_to_mavros node to get information such as the position from gazebo transported to the PX4 Firmware
+*   Nils Rottmann
+*   Nils.Rottmann@tuhh.de
+*/
+
+
 #include "ros/ros.h"
-#include <gazebo_msgs/ModelStates.h>
+#include <gazebo_msgs/ModelStates.h>                // including the required messages
 #include <geometry_msgs/PoseStamped.h>
 
 /*
-This topic subscribes to the gazebo messags ModelStates to get the state of the different models in the gazebo simulation and publishes it to ...
+This topic subscribes to the gazebo ros topic /gazebo/model_states to get the state of the different models in the gazebo simulation
+and publishes it to the /mavros/mocap/pose topic of the mavros package.
 */
 
 //using global coordinates to store the subscribed data
@@ -17,12 +25,12 @@ void modelStatesCallback(const gazebo_msgs::ModelStates::ConstPtr& msg)
     msg_out.pose.position.y = msg->pose[i].position.y;
     msg_out.pose.position.z = msg->pose[i].position.z;
 
-    // this can be used for debugging
+    /*// this can be used for debugging
     ROS_INFO("%s positions:\t%8.4f\t%8.4f\t%8.4f",
                 msg->name[i].c_str(),
                 msg->pose[i].position.x,
                 msg->pose[i].position.y,
-                msg->pose[i].position.z);
+                msg->pose[i].position.z);*/
 }
 
 int main(int argc, char **argv)
@@ -32,18 +40,18 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     // define rate of publishing and subscribing
-    ros::Rate r(20);       // 1 Hz
+    ros::Rate r(20);    // 20 Hz
 
-    // subscriber
-    ros::Subscriber sub = n.subscribe("gazebo/model_states", 1, modelStatesCallback);   // subscriber to get the gazebo model position
+    // Subscriber
+    ros::Subscriber sub = n.subscribe("gazebo/model_states", 1, modelStatesCallback);
     
     // Publisher
     ros::Publisher pub_mocap = n.advertise<geometry_msgs::PoseStamped>("mavros/mocap/pose", 1);
 
     // loop which only ends if this node is killed or ROS is ended
     while(ros::ok()){
-        pub_mocap.publish(msg_out);
-        ros::spinOnce();
+        pub_mocap.publish(msg_out);		// publishing
+        ros::spinOnce();			// subscribing
         r.sleep();
     }
 
